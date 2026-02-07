c1db096da27b3e416966c2f3b2c22c2199b20efd
HinmY <HinmY@DESKTOP-D0HIA51>
2026-02-07 15:05:38 +0800

    昨天项目

--- Pandas_Project/Pandas_Project.py
+++ Pandas_Project/Pandas_Project.py
@@ -42,20 +42,59 @@ df_clean["order_time"]=pd.to_datetime(df_clean["order_time"])
 df_clean["order_year"]=df_clean["order_time"].dt.year
 df_clean["order_month"]=df_clean["order_time"].dt.month
 df_clean["order_day"]=df_clean["order_time"].dt.day
-print("日期类型转换完成，新增年月日字段")
-#.unique()是pandas获取列的唯一值的常用方法
-
-print("\n商品品类唯一值：",df_clean["product_type"].unique())
-print("支付方式唯一值：",df_clean["pay_type"].unique())
-
-print("\n"+"="*50,"清洗后数据检查","="*50)
-print(f"空值数量：{df_clean.isnull().sum()}")
-print(f"重复值数量：{df_clean.duplicated().sum()}")
-df_clean.info()
+# print("日期类型转换完成，新增年月日字段")
+# #.unique()是pandas获取列的唯一值的常用方法
+#
+# print("\n商品品类唯一值：",df_clean["product_type"].unique())
+# print("支付方式唯一值：",df_clean["pay_type"].unique())
+#
+# print("\n"+"="*50,"清洗后数据检查","="*50)
+# print(f"空值数量：{df_clean.isnull().sum()}")
+# print(f"重复值数量：{df_clean.duplicated().sum()}")
+# df_clean.info()
 
 
 # 特征工程：基于清洗后的数据创建衍生特征
 df_analysis=df_clean.copy()
 
 # 按用户分组，计算每个用户的消费频次和客单价（核心：groupby+聚合)
-user_analysis=df_analysis.groupby("user_id")
+user_analysis=df_analysis.groupby("user_id").agg({
+    "order_id":"count",
+    "pay_amount":["sum","mean"]
+
+}).reset_index()
+# 重命名列
+user_analysis.columns=["user_id","order_count","total_amount","avg_amount"]
+# print("="*50,"用户消费特征","="*50)
+# print(user_analysis.head(10).round(2))
+
+
+# 按月份
+month_analysis=df_analysis.groupby("order_month").agg({
+    "order_id":"count",
+    "pay_amount":"sum"
+}).reset_index()
+# print(month_analysis)
+month_analysis.columns=["month","order_num","total_amount"]
+# print("="*50,"月度消费特征","="*50)
+# print(month_analysis.head(10).round(2))
+
+
+# 按商品种类
+product_analysis=df_analysis.groupby("product_type").agg({
+    "order_id":"count",
+    "pay_amount":"sum"
+}).reset_index()
+# print(product_analysis)
+product_analysis.columns=["product","order_num","total_amount"]
+# print("\n" + "="*50, "品类消费特征", "="*50)
+# print(product_analysis.head(10).round(2))
+
+
+print(df_analysis.head(10).round(2).to_string())
+df_analysis=pd.merge(df_analysis,user_analysis,on="user_id",how="left")
+print("="*50,"===========")
+# df_analysis.info()
+
+print("="*50,"===========")
+print(df_analysis.head(10).round(2).to_string())
